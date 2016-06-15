3c4173ac0d45bc670846339de50b6a2a02dd0459
glame <glame@futu5.com>
2016-06-15 22:45:01 +0800

    commit

--- db_util.py
+++ db_util.py
@@ -25,7 +25,7 @@ class db_util:
         self.session = None
 
     def get_db_session(self):
-        engine = create_engine('mysql+pymysql://order_user:order123@localhost:3306/orderdb')
+        engine = create_engine('mysql+pymysql://root:root123@localhost:3306/orderdb')
         db_session = sessionmaker(bind=engine)
         return db_session
 
@@ -36,6 +36,16 @@ class db_util:
     def close_db(self):
         self.session.close()
 
+    def get_todo_orders(self):
+        order_list = self.session.query(cond_order).filter(cond_order.state == 0).all()
+        print order_list
+        return order_list
+
+    def get_cond_order_bystock(self, stock_code):
+        order_list = self.session.query(cond_order).filter(cond_order.stock_code==stock_code).all()
+        print order_list
+        return order_list
+
     def add_condition_order(self, stock_code, direction, action, amount, compare_price, begin_in_day, end_in_day):
         #insert into db
         print "stock_code=", stock_code, ", direction=", direction, ", action=", action,  ", amount=",  amount, ", compare_price=", compare_price, ", begin_in_day=", begin_in_day, ", end_in_day=", end_in_day
@@ -44,10 +54,7 @@ class db_util:
         self.session.add(new_order)
         #print new_order.stock_code
         ret = self.session.commit()
-
-        return ret
-        #order_list = session.query(cond_order).filter(cond_order.stock_code==stock_code).all()
-        #print order_list
+        return 1
 
     def update_cond_order(self, order_id, state):
         self.session.query(cond_order).filter(cond_order.order_id  == order_id).update({cond_order.state: state})
@@ -56,5 +63,6 @@ class db_util:
 
 db_util1 = db_util()
 db_util1.init_db()
-#db_util1.add_condition_order("600036", 1, 1, 100, 17, 2, 3)
+db_util1.get_cond_order("600036")
+db_util1.add_condition_order("600036", 1, 1, 100, 17, 2, 3)
 db_util1.update_cond_order(2, 1)
--- process_cond_order.py
+++ process_cond_order.py
@@ -1,5 +1,12 @@
 import threading
 import time
+from db_util import *
+
+order_direction_def = {"up":1, "down":2}
+order_state_def = {"todo":0, "done":1, "cancel":2}
+order_action_def = {"buy":1, "sell":2}
+
+
 
 class ProcessCondOrder(threading.Thread):
     def __init__(self, proc_id):
@@ -7,22 +14,32 @@ class ProcessCondOrder(threading.Thread):
         self.proc_id = proc_id #进程的id
         self.thread_stop = False #进程是否需要停止
         self.last_dbcheck_time = time.time() #最后一次查看db的时间
-
+        self.todo_orders = None  #所有待触发的订单
 
     #检查价格并且下单
     def check_price_do(self):
+        for row in self.todo_orders:
+            now_price = get_now_price(row.stock_code)
+            if now_price >= row.compare_price and  row.direction == order_direction_def["up"]:
+                auto_trade.buy_sell()
+
+
 
 
     #从数据库load数据到本地进行访问
     def load_from_db(self):
-
+        order_db = db_util()
+        order_db.init_db()
+        self.todo_orders = order_db.get_todo_orders()
+        for row in self.todo_orders:
+            print row.insert_time
 
     def run(self):
         while not self.thread_stop:
             if self.last_dbcheck_time + 60 < time.time():
-                load_from_db()
+                self.load_from_db()
                 self.last_dbcheck_time = time.time()
-            check_price_do();
+            self.check_price_do();
 
     def stop(self):
         self.thread_stop = True
