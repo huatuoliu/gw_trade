357655f24489f4e87c8766493b8eb8926c045a48
glame <stormtrader@sina.com>
2016-06-28 19:22:42 +0800

    commit

--- trade_util.py
+++ trade_util.py
@@ -12,6 +12,8 @@ from stock_util import *
 from html_parser import *
 from crack_bmp import *
 
+
+
 class auto_trade:
     def __init__(self, config_file):
         self.account = ""
@@ -109,10 +111,9 @@ class auto_trade:
         #150906130资金不足
         #150906135股数不够
         #长城的出错都是这个鸟样 alert("-990297020[-990297020]，出错了就反馈空的订单号，看看是不是自己定义一些exception来搞
-        reg = re.compile(ur'alert.*?(-\d+)')
+        reg = re.compile(ur'.*alert.*\[-(\d{6,})\]')
         match = reg.search(ret)
         if match:
-            print "Deal Order Fail"
             return ""
 
         #ok，没有问题
